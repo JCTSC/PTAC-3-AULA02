71b6f311835160e64e8922a3a4a1cfaa57a8991e
JCTSC <jctscamargo@gmail.com>
2024-06-07 17:12:27 -0300

    progress idk
    07.06

--- src/app/componentes/main.js
+++ src/app/componentes/main.js
@@ -1,10 +1,32 @@
 import Image from "next/image";
 import styles from "../componentes/main.module.css";
 import autoprefixer from "autoprefixer";
+import { useEffect } from "react";
 
 
-export default async function Home() {
+export default async function Main() {
     const response = await fetch("https://fakestoreapi.com/products")
+
+      const {listaProduct, setLisProduct} = useState([]);
+    
+      useEffect ( ()=> {
+
+        const getProduct = async () >={
+          const response = await fetch("https://fakestoreapi.com/products");
+          const data = await response.json();
+
+          setLisProduct(data);
+        }
+        getProduct();,
+      }, []);
+
+
+const orderZa = () =>{
+  let newLis 
+}
+
+
+
     const data = await response.json();
   return (
     <main className={styles.main}>
@@ -23,3 +45,4 @@ export default async function Home() {
     </main>
   );
 }
+
