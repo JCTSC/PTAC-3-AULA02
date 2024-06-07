69f16f0e8c634bb87758f10f81199cd0746e8cb9
JCTSC <jctscamargo@gmail.com>
2024-06-07 14:03:19 -0300

    finalizações
    07.06

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import styles from "../componentes/header.modules.css";
 import Image from "next/image";
+import styles from './header.module.css'
 
 export default function Footer(){
 return(
--- src/app/componentes/Header.js
+++ src/app/componentes/Header.js
@@ -1,19 +1,19 @@
 import Link from "next/link"
-import styles from "../componentes/header.modules.css";
 import Image from "next/image";
+import styles from "./header.module.css";
 
 export default function Header(){
-return(
-    <header className={styles.header}>
-         <h1>Header</h1>
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Registrar</li>
-                <li>Localização</li>
-            </ul>
-        </nav>
-    </header>
-)
+    return (
+        <header className={styles.header}>
+            <h1>Header</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Registrar</li>
+                    <li>Localização</li>
+                </ul>
+            </nav>
+        </header>
+    )
 
 }
